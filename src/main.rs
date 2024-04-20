c126da26733e377884b337f5c24d443b595873c4
Alejandro Salas <asalasledezma16@gmail.com>
2024-04-20 13:06:54 -0400

    Add clap to dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
+use clap::{command, Parser};
 use filetime::FileTime;
 use std::fs;
 
-fn main() {
-    let metadata = fs::metadata("tests/20230224_182502.jpg").unwrap();
-    println!("Raw file metadata: {:?}", metadata);
+#[derive(Parser)]
+#[command(version, about)]
+struct Args {}
 
-    let mtime = FileTime::from_last_modification_time(&metadata);
-    println!("From last mod time: {:?}", mtime);
-    println!("{}", mtime.seconds());
-}
+fn main() {}
