c51f5471200c42f7b112c1ec41428233145334c2
Alejandro Salas <asalasledezma16@gmail.com>
2024-05-03 22:18:51 -0400

    Improved files handling in main to update metadata of correctly extracted datetimes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 
 use clap::{command, Parser};
-use telegram_image_metadata::{read_image_path, update_time_metadata};
+use telegram_image_metadata::{extract_datetime, read_image_path, update_time_metadata};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -15,8 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let files = read_image_path(&args.path)?;
 
-    files
-        .into_iter()
-        .for_each(|file| update_time_metadata(file).unwrap());
+    files.into_iter().for_each(|file_path| {
+        if let Some(extracted_datetime) = extract_datetime(&file_path) {
+            update_time_metadata(file_path, extracted_datetime).unwrap()
+        }
+    });
     Ok(())
 }
